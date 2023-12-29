00827d0f474730a6dd3b38aa12a28853c66ecd81
Terje Kvernes <terje@kvernes.no>
2023-12-29 14:03:47 +0100

    Hopefully argon2 doesn't fail.

--- src/utilities/auth.rs
+++ src/utilities/auth.rs
@@ -19,7 +19,8 @@ pub fn hash_password(password: &str) -> Result<String, Box<dyn std::error::Error
     let salt = SaltString::generate(&mut OsRng);
     let argon2 = Argon2::default();
     let password_hash = argon2
-        .hash_password(password.as_bytes(), &salt)?
+        .hash_password(password.as_bytes(), &salt)
+        .unwrap()
         .to_string();
 
     Ok(password_hash)
