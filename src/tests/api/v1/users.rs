213a1bf89de6378cdf491288f91ca14922dd73cd
Terje Kvernes <terje@kvernes.no>
2024-01-11 02:04:47 +0100

    Add support for post and delete operations...
      - And use them to test the users endpoint.

--- src/tests/api/v1/users.rs
+++ src/tests/api/v1/users.rs
@@ -1,9 +1,9 @@
 #[cfg(test)]
 mod tests {
-    use crate::models::user::User;
+    use crate::models::user::{NewUser, User};
     use actix_web::{http::StatusCode, test};
 
-    use crate::tests::api_operations::send_get_request;
+    use crate::tests::api_operations::{send_delete_request, send_get_request, send_post_request};
     use crate::tests::asserts::assert_response_status;
     use crate::tests::{create_test_admin, create_test_user, setup_pool_and_admin_user};
 
@@ -62,5 +62,49 @@ mod tests {
         check_show_user_tokens(&pool, &test_user, &test_user, StatusCode::OK).await;
         check_show_user_tokens(&pool, &test_admin_user, &test_user, StatusCode::FORBIDDEN).await;
         check_show_user_tokens(&pool, &test_user, &test_admin_user, StatusCode::OK).await;
+    }
+
+    #[actix_web::test]
+    async fn test_create_and_delete_user() {
+        let (pool, admin_token) = setup_pool_and_admin_user().await;
+
+        let normal_token = create_test_user(&pool)
+            .add_token(&pool)
+            .unwrap()
+            .get_token();
+
+        let new_user = NewUser {
+            username: "test_create_user_endpoint".to_string(),
+            password: "testpassword".to_string(),
+            email: None,
+        };
+
+        // Just checking that only admins can create users...
+        let resp = send_post_request(&pool, &normal_token, USERS_ENDPOINT, &new_user).await;
+        let _ = assert_response_status(resp, StatusCode::FORBIDDEN).await;
+
+        let resp = send_post_request(&pool, &admin_token, USERS_ENDPOINT, &new_user).await;
+        let resp = assert_response_status(resp, StatusCode::CREATED).await;
+
+        let created_user_url = resp.headers().get("Location").unwrap().to_str().unwrap();
+        let resp = send_get_request(&pool, &admin_token, created_user_url).await;
+        let resp = assert_response_status(resp, StatusCode::OK).await;
+        let created_user: User = test::read_body_json(resp).await;
+
+        // Validate that the location is what we expect
+        assert_eq!(
+            created_user_url,
+            &format!("{}/{}", USERS_ENDPOINT, created_user.id)
+        );
+
+        // And only admins can delete users...
+        let resp = send_delete_request(&pool, &normal_token, &created_user_url).await;
+        let _ = assert_response_status(resp, StatusCode::FORBIDDEN).await;
+
+        let resp = send_delete_request(&pool, &admin_token, &created_user_url).await;
+        let _ = assert_response_status(resp, StatusCode::NO_CONTENT).await;
+
+        let resp = send_get_request(&pool, &admin_token, created_user_url).await;
+        let _ = assert_response_status(resp, StatusCode::NOT_FOUND).await;
     }
 }
--- src/tests/api_operations.rs
+++ src/tests/api_operations.rs
@@ -1,8 +1,10 @@
 use crate::api as prod_api;
+use crate::db::connection::DbPool;
 use actix_web::{http, test, web, App};
+use serde::Serialize;
 
 pub async fn send_get_request(
-    app: &web::Data<crate::db::connection::DbPool>,
+    app: &web::Data<DbPool>,
     token: &str,
     endpoint: &str,
 ) -> actix_web::dev::ServiceResponse {
@@ -10,6 +12,41 @@ pub async fn send_get_request(
         test::init_service(App::new().app_data(app.clone()).configure(prod_api::config)).await;
 
     test::TestRequest::get()
+        .insert_header((http::header::AUTHORIZATION, format!("Bearer {}", token)))
+        .uri(endpoint)
+        .send_request(&app)
+        .await
+}
+
+pub async fn send_post_request<T>(
+    app: &web::Data<DbPool>,
+    token: &str,
+    endpoint: &str,
+    content: T,
+) -> actix_web::dev::ServiceResponse
+where
+    T: Serialize,
+{
+    let app =
+        test::init_service(App::new().app_data(app.clone()).configure(prod_api::config)).await;
+
+    test::TestRequest::post()
+        .insert_header((http::header::AUTHORIZATION, format!("Bearer {}", token)))
+        .uri(endpoint)
+        .set_json(&content) // Make sure to reference content
+        .send_request(&app)
+        .await
+}
+
+pub async fn send_delete_request(
+    app: &web::Data<DbPool>,
+    token: &str,
+    endpoint: &str,
+) -> actix_web::dev::ServiceResponse {
+    let app =
+        test::init_service(App::new().app_data(app.clone()).configure(prod_api::config)).await;
+
+    test::TestRequest::delete()
         .insert_header((http::header::AUTHORIZATION, format!("Bearer {}", token)))
         .uri(endpoint)
         .send_request(&app)
